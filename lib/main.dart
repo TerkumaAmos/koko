e115a1f797da74adfbbb27e48176cbb0e7954bc3
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-11 23:24:15 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -10,42 +10,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-
-                  // decoration: const BoxDecoration(
-                  //   color: Colors.amber,
-                  //   shape: BoxShape.rectangle,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      // ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Container(
+//                   height: 300,
+//                   width: 300,
+//                   decoration: BoxDecoration(
+//                     color: Colors.red,
+//                     borderRadius: BorderRadius.circular(30),
+//                   ),
+
+//                   // decoration: const BoxDecoration(
+//                   //   color: Colors.amber,
+//                   //   shape: BoxShape.rectangle,
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 Container(
+//                   height: 300,
+//                   width: 300,
+//                   decoration: BoxDecoration(
+//                     color: Colors.black,
+//                     borderRadius: BorderRadius.circular(30),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//       // ),
+//     );
+//   }
+// }
