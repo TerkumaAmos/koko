1b22b4aea83b37aaa041e73a1d24f74754b0744f
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-11 23:29:22 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+        ),
+      ),
     );
   }
 }
